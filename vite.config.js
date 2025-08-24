67ac670f98a601b97f39177a7d922b55d1dcb289
russellcolestock <92215495+russellcolestock@users.noreply.github.com>
2025-08-24 08:54:48 -0500

    Updates to package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "Shopping List",
+        short_name: "ShopList",
+        theme_color: "#EEF0F4",
+        background_color: "#EEF0F4",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ],
 })
+
