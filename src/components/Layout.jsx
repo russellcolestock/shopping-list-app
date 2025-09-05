9db8f25ebc60c74d77ccd8f1eeffbee5d0d04047
russellcolestock <92215495+russellcolestock@users.noreply.github.com>
2025-09-05 14:54:31 -0500

    multiple changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,12 +4,12 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
     return (
-        <>
+        <div className="layout">
             <Header />
             <main>
                 <Outlet />
             </main>
             <Footer />
-        </>
+        </div>
     );
 }
